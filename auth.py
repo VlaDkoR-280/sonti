97266f661ae85ae4c49684223603172c5e3fbd8c
VlaDkoR-280 <vladkor280@yandex.ru>
2023-11-10 19:08:03 +0300

    Auth system is worked

--- auth.py
+++ auth.py
@@ -6,11 +6,17 @@ from telethon.tl.types import InputPeerEmpty
 
 data = ["Null", "Null", "Null"] # phone, api_id, api_hash
 
-with open('info.txt', 'r') as f:
-    for i in range(3):
-        data[i] = f.readline().split('\n')[0]
+def Start():
+    with open('info.txt', 'r') as f:
+        for i in range(3):
+            data[i] = f.readline().split('\n')[0]
 
-client = TelegramClient(data[0], data[1], data[2])
+    client = TelegramClient(data[0], data[1], data[2])
 
-client.start()
+    client.start()
 
+    result = client(GetDialogsRequest(offset_date=None, offset_id=0, offset_peer=InputPeerEmpty(), limit=200, hash = 0))
+    chats = []
+
+    chats.extend(result.chats)
+    return chats
