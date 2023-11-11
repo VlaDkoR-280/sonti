94c841264f6560f9379b2d60ad0ddedf2c4e9ace
Alex-bit-rus <aleks-mayorov@inbox.ru>
2023-11-11 22:21:32 +0300

    Add function status

--- auth.py
+++ auth.py
@@ -20,3 +20,18 @@ def Start():
 
     chats.extend(result.chats)
     return chats
+
+def status(username):
+	data = ["Null", "Null", "Null"]
+	with open('info.txt', 'r') as f:
+		for i in range(3):
+			data[i] = f.readline().split('\n')[0]
+	client = TelegramClient(data[0], data[1], data[2])
+	client.start()
+	entity = client.get_entity(username)
+	try:
+		entity = client.get_entity(username)
+		print(str(entity.status)[:str(entity.status).find('(')])
+	except Exception as e:
+		print(f'Не удалось получить информацию о пользователе: {e}')
+	client.disconnect()
