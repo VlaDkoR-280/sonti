72e8f08e33826f4133b893fc25c97ac2229d9ee3
VlaDkoR-280 <vladkor280@yandex.ru>
2023-11-12 19:23:09 +0300

    Create Setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='sonti'
+        version='1.0'
+        description='Module for detecting activity in a telegram'
+        author=''
+        author_email=''
+        packages=['sonti']
+        install_requires=['pandas', 'telethon', 'time', 'datetime'],
+)
